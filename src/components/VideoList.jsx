d0d9f2c289b264b76bae569f0d2dcd255a93d227
trautlein <hans@trautlein.com>
2016-09-30 15:47:09 -0700

    Dynamic Video List Component Complete w/ exampleVideoData

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,16 +1,9 @@
-var VideoList = () => (
+var VideoList = (props) => (
   <div className="video-list media">
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
+    {props.videos.map(item => <VideoListEntry video={item} />)}
   </div>
 );
+
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
@@ -21,3 +14,4 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 window.VideoList = VideoList;
+
