505fde65e63c3d3d811f7c69574b0b914bdee9ac
trautlein <hans@trautlein.com>
2016-09-30 20:04:13 -0700

    late nite commit

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,12 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  var mySearch = {};
+  mySearch.key = options.key;
+  mySearch.q = options.query;
+  mySearch.maxResults = options.max;
+  $.get('https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&videoEmbeddable=true', mySearch, function (data) {
+    console.log(data.items);
+    return callback(data.items);
+  });
 };
 
 window.searchYouTube = searchYouTube;
